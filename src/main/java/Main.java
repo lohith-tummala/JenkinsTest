c8867f021fd4e2c3ec9d3bc76455201519a99da9
Lohith <lohithtummala45@gmail.com>
2019-04-04 16:57:39 +0530

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = args[0];
+
+        System.setProperty("webdriver.chrome.driver", "/Users/lohith.tummala/work/JarTest/src/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+        WebElement elem = driver.findElement(By.name("q"));
+        elem.sendKeys(s);
+        elem.sendKeys(Keys.RETURN);
+
+    }
+}
