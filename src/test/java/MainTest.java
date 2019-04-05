e1a4166009a2f9e54b66bb4e8a8a01b31a862b57
Lohith <lohithtummala45@gmail.com>
2019-04-05 11:03:01 +0530

    Test Commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testMain(){
+        String[] args = {"gmail"};
+        Main main = new Main();
+        main.main(args);
+    }
+}
+
